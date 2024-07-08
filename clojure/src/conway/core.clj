5cb6a7c65516e1e619a6738c95757d4403ff6db8
William Wingate <wtwingate@gmail.com>
2024-07-07 23:09:13 -0400

    Write function to create new grid based on game rules

--- clojure/src/conway/core.clj
+++ clojure/src/conway/core.clj
@@ -1,33 +1,40 @@
 (ns conway.core)
 
-(defn -main
-  "I don't do a lot... yet!"
-  [& args]
-  (print-grid (create-random-grid 5)))
-
 (defn create-random-column
   [size]
   (into [] (repeatedly size #(rand-nth '(" " "#")))))
 
 (defn create-random-grid
   [size]
-  (into [] (repeatedly size #(create-column size))))
+  (into [] (repeatedly size #(create-random-column size))))
 
 (defn print-grid
   [grid]
   (print (reduce str (map (fn [column] (str (reduce str column) "\n")) grid))))
 
+(defn get-neighbor-coords
+  [grid x y]
+  (let [x-max (- (count grid) 1)
+        y-max (- (count (get grid 0)) 1)
+        deltas [[0 1] [1 1] [1 0] [1 -1] [0 -1] [-1 -1] [-1 0] [-1 1]]]
+    (map (fn [delta]
+           (let [x-coord (+ x (get delta 0))
+                 y-coord (+ y (get delta 1))]
+             (conj (vector)
+                   (cond
+                     (< x-coord 0) x-max
+                     (> x-coord x-max) 0
+                     :else x-coord)
+                   (cond
+                     (< y-coord 0) y-max
+                     (> y-coord y-max) 0
+                     :else y-coord)))) deltas)))
+
 (defn count-live-neighbors
   [grid x y]
-  (let [neighbors (conj (vector)
-                        (get-in grid [(+ x 0) (+ y 1)])
-                        (get-in grid [(+ x 1) (+ y 1)])
-                        (get-in grid [(+ x 1) (+ y 0)])
-                        (get-in grid [(+ x 1) (- y 1)])
-                        (get-in grid [(+ x 0) (- y 1)])
-                        (get-in grid [(- x 1) (- y 1)])
-                        (get-in grid [(- x 1) (+ y 0)])
-                        (get-in grid [(- x 1) (+ y 1)]))]
+  (let [neighbor-coords (get-neighbor-coords grid x y)
+        neighbors (map (fn [coords]
+                         (get-in grid coords)) neighbor-coords)]
     (count (filter #(= "#" %) neighbors))))
 
 (defn generate-new-grid
@@ -37,9 +44,14 @@
                                 (let [live-neighbors (count-live-neighbors grid x-idx y-idx)]
                                   (if (= "#" cell)
                                     (cond
-                                      (< 2 live-neighbors) " "
-                                      (> 3 live-neighbors) " "
+                                      (< live-neighbors 2) " "
+                                      (> live-neighbors 3) " "
                                       :else "#")
                                     (cond
-                                      (= 3 live-neighbors) "#"
-                                      :else " "))))) column) grid))
+                                      (= live-neighbors 3) "#"
+                                      :else " ")))) column)) grid))
+
+(defn -main
+  "I don't do a lot... yet!"
+  [& args]
+  (print-grid (generate-new-grid (create-random-grid 5))))
