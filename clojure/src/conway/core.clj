6bf1ac35a4030fa641ee895f5b2f31ed5962fe85
William Wingate <wtwingate@gmail.com>
2024-07-08 21:03:01 -0400

    Finish making the game of life in Clojure

--- clojure/src/conway/core.clj
+++ clojure/src/conway/core.clj
@@ -1,20 +1,16 @@
 (ns conway.core)
 
-
 (defn create-random-column
-  [size]
-  (into [] (repeatedly size #(rand-nth '(" " "#")))))
-
+  [y-size]
+  (into [] (repeatedly y-size #(rand-nth '(" " "#")))))
 
 (defn create-random-grid
-  [size]
-  (into [] (repeatedly size #(create-random-column size))))
-
+  [x-size y-size]
+  (into [] (repeatedly x-size #(create-random-column y-size))))
 
 (defn print-grid
   [grid]
   (print (reduce str (map (fn [column] (str (reduce str column) "\n")) grid))))
-
 
 (defn get-neighbor-coords
   [grid x y]
@@ -34,7 +30,6 @@
                      (> y-coord y-max) 0
                      :else y-coord)))) deltas)))
 
-
 (defn count-live-neighbors
   [grid x y]
   (let [neighbor-coords (get-neighbor-coords grid x y)
@@ -42,28 +37,29 @@
                          (get-in grid coords)) neighbor-coords)]
     (count (filter #(= "#" %) neighbors))))
 
-
 (defn next-generation
   [grid]
   (into [] (map-indexed (fn [x-idx column]
-                         (into [] (map-indexed (fn [y-idx cell]
-                                                 (let [live-neighbors (count-live-neighbors grid x-idx y-idx)]
-                                                   (if (= "#" cell)
-                                                     (cond
-                                                       (< live-neighbors 2) " "
-                                                       (> live-neighbors 3) " "
-                                                       :else "#")
-                                                     (cond
-                                                       (= live-neighbors 3) "#"
-                                                       :else " ")))) column))) grid)))
-
+                          (into [] (map-indexed (fn [y-idx cell]
+                                                  (let [live-neighbors (count-live-neighbors grid x-idx y-idx)]
+                                                    (if (= "#" cell)
+                                                      (cond
+                                                        (< live-neighbors 2) " "
+                                                        (> live-neighbors 3) " "
+                                                        :else "#")
+                                                      (cond
+                                                        (= live-neighbors 3) "#"
+                                                        :else " ")))) column))) grid)))
 
 (defn game-of-life
-  [grid]
-  (print-grid (first (iterate next-generation grid))))
-
+  [x-size y-size]
+  (let [grid (atom (create-random-grid x-size y-size))]
+    (while true
+      (print-grid @grid)
+      (println "================================================================================")
+      (Thread/sleep 500)
+      (reset! grid (next-generation @grid)))))
 
 (defn -main
-  [& args]
-  (let [grid (create-random-grid 10)]
-    (game-of-life grid)))
+  []
+  (game-of-life 20 80))
