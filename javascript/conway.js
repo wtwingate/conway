3f0e885a57a4708aedec90b13dcacd0ad4e6f285
William Wingate <wtwingate@gmail.com>
2024-07-14 23:34:23 -0400

    Refactor Conway's Game of Life in JavaScript

--- javascript/conway.js
+++ javascript/conway.js
@@ -9,125 +9,99 @@ const DELTAS = [
   [-1, 1],
 ];
 
-class Grid {
-  constructor() {
-    this.width = 80;
-    this.height = 64;
-    this.size = 10;
+class Conway {
+  constructor(width, height, size) {
+    this.width = width;
+    this.height = height;
+    this.size = size;
     this.speed = 200;
-    this.cells = this.init();
-    this.prev = null;
+    this.grid = this.create();
   }
 
-  init() {
-    const cellArray = [];
+  create() {
+    const grid = [];
     for (let i = 0; i < this.width; i++) {
-      const cellColumn = [];
+      const column = [];
       for (let j = 0; j < this.height; j++) {
-        const cell = new Cell(i, j, Math.random() < 0.5);
-        cellColumn.push(cell);
+        const cell = Math.random() < 0.5;
+        column.push(cell);
       }
-      cellArray.push(cellColumn);
+      grid.push(column);
     }
-    return cellArray;
+    return grid;
   }
 
   update() {
-    this.prev = structuredClone(this.cells);
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        const cell = this.cells[i][j];
-        const numAliveNeighbors = this.aliveNeighbors(i, j);
-        cell.update(numAliveNeighbors);
+    const newGrid = [];
+    for (let x = 0; x < this.width; x++) {
+      const newColumn = [];
+      for (let y = 0; y < this.height; y++) {
+        const cell = this.grid[x][y];
+        const numAliveNeighbors = this.aliveNeighbors(x, y);
+        newColumn.push(this.liveOrDie(cell, numAliveNeighbors));
       }
+      newGrid.push(newColumn);
     }
+    this.grid = newGrid;
   }
 
   aliveNeighbors(x, y) {
-    const cell = this.prev[x][y];
     let numAliveNeighbors = 0;
     for (const d of DELTAS) {
       // check for horizontal wrap
-      let nx = x + d[0];
-      if (nx < 0) {
-        nx = this.width + nx;
-      } else if (nx >= this.width) {
-        nx = 0 + (nx - this.width);
+      let neighborX = x + d[0];
+      if (neighborX < 0) {
+        neighborX = this.width + neighborX;
+      } else if (neighborX >= this.width) {
+        neighborX = 0 + (neighborX - this.width);
       }
-
       // check for vertical wrap
-      let ny = y + d[1];
-      if (ny < 0) {
-        ny = this.height + ny;
-      } else if (ny >= this.height) {
-        ny = 0 + (ny - this.height);
+      let neighborY = y + d[1];
+      if (neighborY < 0) {
+        neighborY = this.height + neighborY;
+      } else if (neighborY >= this.height) {
+        neighborY = 0 + (neighborY - this.height);
       }
 
-      if (this.prev[nx][ny].alive === true) {
+      if (this.grid[neighborX][neighborY] === true) {
         numAliveNeighbors++;
       }
     }
     return numAliveNeighbors;
   }
 
+  liveOrDie(cell, n) {
+    if (cell) {
+      return n == 2 || n == 3;
+    } else {
+      return n == 3;
+    }
+  }
+
   draw() {
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        const x = i * this.size;
-        const y = j * this.size;
-        if (this.cells[i][j].alive === true) {
-          context.fillRect(x, y, this.size, this.size);
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        const x_coord = x * this.size;
+        const y_coord = y * this.size;
+        if (this.grid[x][y]) {
+          context.fillRect(x_coord, y_coord, this.size, this.size);
         } else {
-          context.clearRect(x, y, this.size, this.size);
+          context.clearRect(x_coord, y_coord, this.size, this.size);
         }
       }
     }
   }
 
-  loop() {
+  play() {
     setTimeout(() => {
       this.draw();
       this.update();
-      this.loop();
-    }, this.speed);
+      this.play();
+    }, 250);
   }
 }
 
-class Cell {
-  constructor(x, y, alive) {
-    this.x = x;
-    this.y = y;
-    this.alive = alive;
-  }
-
-  update(numAliveNeighbors) {
-    if (this.alive === true) {
-      if (numAliveNeighbors < 2) {
-        // Any live cell with fewer than two live neighbors dies,
-        // as if by underpopulation.
-        this.alive = false;
-      } else if (numAliveNeighbors < 4) {
-        // Any live cell with two or three live neighbors lives on
-        // to the next generation.
-        this.alive = true;
-      } else {
-        // Any live cell with more than three live neighbors dies,
-        // as if by overpopulation.
-        this.alive = false;
-      }
-    } else {
-      if (numAliveNeighbors === 3) {
-        // Any dead cell with exactly three live neighbors becomes
-        // alive, as if by reproduction.
-        this.alive = true;
-      } else {
-        this.alive = false;
-      }
-    }
-  }
-}
-
-const grid = new Grid();
-grid.loop();
+const game = new Conway(80, 64, 10);
+game.play();
